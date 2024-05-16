d4a17846108693b1c36f7624aa7d04aea787b3f5
avital.mozes <avitalmoz@gmail.com>
2024-05-16 11:05:22 +0300

    upload src

---
+++ src/cripty.cpp
@@ -0,0 +1,46 @@
+#include "cripty.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+namespace cripty
+{
+
+CriptyInfectedFilesFinder::CriptyInfectedFilesFinder()
+{}
+
+CriptyInfectedFilesFinder::~CriptyInfectedFilesFinder()
+{}
+
+void cripty::CriptyInfectedFilesFinder::searchInfectedFiles(const std::string &root_dir, const std::string &signature)
+{
+    for (const auto& entry : fs::recursive_directory_iterator(root_dir)) 
+    {
+        std::string file_path = entry.path().string();
+
+        if(!fs::is_directory(file_path))
+        {
+            if(isInfected(file_path, signature))
+            {
+                std::cout << "Infected file found: " << file_path << std::endl;
+            }
+        }
+    }
+}
+
+bool CriptyInfectedFilesFinder::isInfected(const std::string &file_path, const std::string &signature)
+{
+    std::ifstream file(file_path, std::ios::binary);
+    if (!file.is_open()) 
+    {
+        return false;
+    }
+
+    std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    return content.find(signature) != std::string::npos;
+}
+
+} // namespace cripty
---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+#include "cripty.hpp"
+
+namespace fs = std::filesystem;
+
+int main(int argc, const char** argv) 
+{ 
+    if (argc < 3) 
+    {
+        std::cerr << "Usage: " << argv[0] << " <root_directory> <signature_file>" << std::endl;
+        return 1;
+    }
+
+    // Check if the root directory exists
+    std::string root_dir = argv[1];
+    if (!fs::is_directory(root_dir)) 
+    {
+        std::cerr << "Error: Root directory does not exist or is not a directory." << std::endl;
+        return 1;
+    }
+
+    // Check if the signature file exists
+    std::string signature_file_path = argv[2];
+    if (!fs::is_regular_file(signature_file_path)) 
+    {
+        std::cerr << "Error: Signature file does not exist or is not a regular file." << std::endl;
+        return 1;
+    }
+    
+    std::ifstream signature_file(signature_file_path);
+    if (!signature_file.is_open()) 
+    {
+        std::cerr << "Error: Unable to open signature file." << std::endl;
+        return 1;
+    }
+
+    std::string signature;
+    signature.assign((std::istreambuf_iterator<char>(signature_file)), (std::istreambuf_iterator<char>()));
+    signature_file.close();
+
+    cripty::CriptyInfectedFilesFinder cripty_obj;
+    cripty_obj.searchInfectedFiles(root_dir, signature);
+    
+    return 0; 
+}
